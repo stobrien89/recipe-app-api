9a7fb39fdb3fb6f54946f8137a5f59b3c76d4067
stobrien89 <stobrien@crimson.ua.edu>
2020-08-28 20:03:12 -0700

    add user model, add tests for creation, email validation

--- app/core/models.py
+++ app/core/models.py
@@ -6,11 +6,14 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         # creates and saves new user
-        user = self.model(email=email, **extra_fields) 
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self._db) 
+        user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     # custom user model that supports using email instead of username
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         email = 'test@test.com'
@@ -12,3 +13,15 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        # checks if email for new user is normalized
+        email = "test@TEST.COM"
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def new_user_invalid_email(self):
+        # raise error on no email
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
